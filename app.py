5cc8679d6e0a482bd836a982fe6a71887e24ff4e
Kamal Khumar <48514949+kamal2230@users.noreply.github.com>
2020-08-04 20:50:03 +0530

    Code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 client=MongoClient("mongodb://localhost:27017/")
 db=client['test']
-#db.Demo.insert_many([{"Name":"Kamal","Age":20},{"Name":"Nishanth","Age":5}])
-#db.Demo.insert_one({"Address":"Sarvam","City":"Chennai"})
+db.Demo.insert_many([{"Name":"Kamal","Age":20},{"Name":"Nishanth","Age":5}])
+db.Demo.insert_one({"Address":"Sarvam","City":"Chennai"})
 for i in db.Demo.find({"Name":{"$regex":"\w"}},{"_id":0}):
 	print(i)
 print(db.Demo.count_documents({"Name":{"$regex":"\w"}})) 
